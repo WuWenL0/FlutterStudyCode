50f440f6f23b2a3d23f136315982bbe914e15d0b
WuWenL0 <wuwenl0405@sina.cn>
2022-09-08 21:48:34 +0800

    basic layout

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ), //注册路由表
+      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -28,14 +28,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,21 +38,170 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have clicked the button this many times:',
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return const ConstrainedBoxPage();
+                  }),
+                );
+              },
+              child: const Text("ConstrainedBox"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return const RowAndColumnPage();
+                  }),
+                );
+              },
+              child: const Text("RowAndColumn"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+}
+
+class RowAndColumnPage extends StatefulWidget {
+  const RowAndColumnPage({super.key});
+
+  @override
+  State<RowAndColumnPage> createState() => _RowAndColumnPageState();
+}
+
+class _RowAndColumnPageState extends State<RowAndColumnPage> {
+  Widget redBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.red));
+  Widget greenBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.green));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("ConstrainedBox & SizeBox"),
+        ),
+        body: Column(
+          children: [
+            Column(
+              //测试Row对齐方式，排除Column默认居中对齐的干扰
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(" hello world "),
+                    Text(" I am Jack "),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(" hello world "),
+                    Text(" I am Jack "),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  textDirection: TextDirection.rtl,
+                  children: <Widget>[
+                    Text(" hello world "),
+                    Text(" I am Jack "),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  verticalDirection: VerticalDirection.up,
+                  children: <Widget>[
+                    Text(
+                      " hello world ",
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                    Text(" I am Jack "),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+}
+
+class ConstrainedBoxPage extends StatefulWidget {
+  const ConstrainedBoxPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ConstrainedBoxState();
+}
+
+class _ConstrainedBoxState extends State<ConstrainedBoxPage> {
+  Widget redBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.red));
+  Widget greenBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.green));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("ConstrainedBox & SizeBox"),
+          actions: const <Widget>[
+            UnconstrainedBox(
+              child: SizedBox(
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(
+                  strokeWidth: 3,
+                  valueColor: AlwaysStoppedAnimation(Colors.white70),
+                ),
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                minWidth: double.infinity,
+                minHeight: 50.0,
+              ),
+              child: Container(
+                height: 80.0,
+                child: redBox,
+              ),
+            ),
+            SizedBox(width: 80.0, height: 80.0, child: greenBox),
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 60.0, minHeight: 60.0), //父
+              child: ConstrainedBox(
+                constraints:
+                    BoxConstraints(minWidth: 90.0, minHeight: 20.0), //子
+                child: redBox,
+              ),
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 90.0, minHeight: 20.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minWidth: 60.0, minHeight: 60.0),
+                child: greenBox,
+              ),
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 60.0, minHeight: 100.0), //父
+              child: UnconstrainedBox(
+                //“去除”父级限制
+                child: ConstrainedBox(
+                  constraints:
+                      BoxConstraints(minWidth: 90.0, minHeight: 20.0), //子
+                  child: redBox,
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }

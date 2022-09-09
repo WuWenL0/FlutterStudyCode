f9bb5a80c2830dbed23d26296f2e2c5c77ee3d8a
WuWenL <wuwenl0405@sina.cn>
2022-09-09 16:33:44 +0800

    布局相关

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -58,8 +58,282 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Text("RowAndColumn"),
             ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return const FlexPage();
+                  }),
+                );
+              },
+              child: const Text("Flex"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return const FlowAndWrapPage();
+                  }),
+                );
+              },
+              child: const Text("FlowAndWrap"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return const StackAndPositionedPage();
+                  }),
+                );
+              },
+              child: const Text("StackAndPositioned"),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class StackAndPositionedPage extends StatefulWidget {
+  const StackAndPositionedPage({super.key});
+
+  @override
+  State<StackAndPositionedPage> createState() => _StackAndPositionedPageState();
+}
+
+class _StackAndPositionedPageState extends State<StackAndPositionedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Stack & Positioned"),
+      ),
+      body: //通过ConstrainedBox来确保Stack占满屏幕
+          ConstrainedBox(
+        constraints: BoxConstraints.expand(),
+        child: Stack(
+          alignment: Alignment.center, //指定未定位或部分定位widget的对齐方式
+          children: <Widget>[
+            Container(
+              child: Text("Hello world", style: TextStyle(color: Colors.white)),
+              color: Colors.red,
+            ),
+            Positioned(
+              left: 18.0,
+              child: Text("I am Jack"),
+            ),
+            Positioned(
+              top: 18.0,
+              child: Text("Your friend"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FlowAndWrapPage extends StatefulWidget {
+  const FlowAndWrapPage({super.key});
+
+  @override
+  State<FlowAndWrapPage> createState() => _FlowAndWrapPageState();
+}
+
+class _FlowAndWrapPageState extends State<FlowAndWrapPage> {
+  Widget redBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.red));
+  Widget greenBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.green));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Flow & Wrap"),
+        ),
+        body: Column(
+          children: [
+            Wrap(
+              spacing: 8.0, // 主轴(水平)方向间距
+              runSpacing: 4.0, // 纵轴（垂直）方向间距
+              alignment: WrapAlignment.center, //沿主轴方向居中
+              children: const <Widget>[
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.blue, child: Text('A')),
+                  label: Text('Hamilton'),
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.blue, child: Text('M')),
+                  label: Text('Lafayette'),
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.blue, child: Text('H')),
+                  label: Text('Mulligan'),
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.blue, child: Text('J')),
+                  label: Text('Laurens'),
+                ),
+              ],
+            ),
+            Flow(
+              delegate: TestFlowDelegate(margin: EdgeInsets.all(10.0)),
+              children: <Widget>[
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  color: Colors.brown,
+                ),
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  color: Colors.purple,
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
+
+class TestFlowDelegate extends FlowDelegate {
+  EdgeInsets margin;
+
+  TestFlowDelegate({this.margin = EdgeInsets.zero});
+
+  double width = 0;
+  double height = 0;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    var x = margin.left;
+    var y = margin.top;
+    //计算每一个子widget的位置
+    for (int i = 0; i < context.childCount; i++) {
+      var w = context.getChildSize(i)!.width + x + margin.right;
+      if (w < context.size.width) {
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
+        x = w + margin.left;
+      } else {
+        x = margin.left;
+        y += context.getChildSize(i)!.height + margin.top + margin.bottom;
+        //绘制子widget(有优化)
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
+        x += context.getChildSize(i)!.width + margin.left + margin.right;
+      }
+    }
+  }
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    // 指定Flow的大小，简单起见我们让宽度竟可能大，但高度指定为200，
+    // 实际开发中我们需要根据子元素所占用的具体宽高来设置Flow大小
+    return Size(double.infinity, 200.0);
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+class FlexPage extends StatefulWidget {
+  const FlexPage({super.key});
+
+  @override
+  State<FlexPage> createState() => _FlexPageState();
+}
+
+class _FlexPageState extends State<FlexPage> {
+  Widget redBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.red));
+  Widget greenBox =
+      const DecoratedBox(decoration: BoxDecoration(color: Colors.green));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ConstrainedBox & SizeBox"),
+      ),
+      body: Column(
+        children: <Widget>[
+          //Flex的两个子widget按1：2来占据水平空间
+          Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: 30.0,
+                  color: Colors.red,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 30.0,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: SizedBox(
+              height: 100.0,
+              //Flex的三个子widget，在垂直方向按2：1：1来占用100像素的空间
+              child: Flex(
+                direction: Axis.vertical,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      height: 30.0,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 30.0,
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
